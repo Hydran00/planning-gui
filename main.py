68b47e2d5b889ea1b92aabd4d8d074898d1a4a14
Davide <ndavide.dn@gmail.com>
2024-01-15 21:59:31 +0100

    Fixed simulation killing

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
-import PySimpleGUI as sg
 import time
 import os
 import subprocess
 import sys
+import numpy as np
+import signal
+import PySimpleGUI as sg
+import psutil
+
+import matplotlib
+from matplotlib.ticker import NullFormatter  # useful for `logit` scale
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+matplotlib.use('TkAgg')
+
+
 # sg.Print('Re-routing the stdout', do_not_reroute_stdout=False)
 debug = sg.Print
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+def kill_gazebo():
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if proc.name() == "gzserver":
+            print('Killing gzserver')
+            proc.kill()
+        if proc.name() == "gzclient":
+            print('Killing gzclient')
+            proc.kill()
 def main():
     # os.system(". ~/shelfino_ws/install/setup.zsh")
     # sg.Print('Re-routing the stdout', do_not_reroute_stdout=False)
-    layout = [  [sg.Text('Welcome to the best planning algorithm GUI ever written :)')],
-                [sg.Output(size=(60,15))],          # an output area where all print output will go
+    layout = [  [sg.Text('Select the planner and click "Launch Planner" ', key='title')],
+                [sg.Canvas(key='-CANVAS-', size=(500, 400)),
+                sg.Output(size=(15,10))],          # an output area where all print output will go
                 [sg.Button('Launch Gazebo Simulation', key='sim', button_color=('white', 'green') ), sg.Button('Exit')],   # a couple of buttons
                 [sg.Text('Select Planner:'),
                 sg.Combo(['RRT','RRT*', 'RRT* Dubins', 'Voronoi'], key='dropdown'),
                 sg.Button('Launch Planner', key='planner', button_color=('white', 'green') )]]    # a couple of buttons
-    window = sg.Window('Planning algorithm test', layout)
+    window = sg.Window('Planning algorithm test', layout, font='Helvetica 18')
 
     env_running = False
+    already_showed = False
     try:
         while True:             # Event Loop
             event, values = window.Read()
@@ -42,7 +71,8 @@ def main():
             
             if event in (None, 'Exit'):         # checks if user wants to exit
                 if(env_running):
-                    p.kill()
+                    os.killpg(os.getpgid(p.pid), signal.SIGTERM) 
+                    kill_gazebo()
                 print('Exiting')
                 break
             
@@ -50,7 +80,7 @@ def main():
                 cmd = "ros2 launch projects victims.launch.py"
                 # cmd = "cat tmp"
                 # runCommand(cmd=values['_IN_'], window=window)
-                p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, preexec_fn=os.setsid)
                 # p = subprocess.run(cmd, shell=True).#, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                 env_running = True
                 window['sim'].update('    Kill simulation           ')
@@ -60,21 +90,33 @@ def main():
                 continue
 
             if event == 'sim' and env_running:
-                debug('Killing simulation')
+                print('Killing simulation')
                 env_running = False
                 window['sim'].update('Launch Gazebo Simulation')
                 window['sim'].update(button_color=('white', 'green'))
-                p.terminate()
-                p.kill()
+                kill_gazebo()
+
+                os.killpg(os.getpgid(p.pid), signal.SIGTERM) 
                 print('Simulation killed')
                 continue
             
+            if event == 'planner' and not already_showed:
+                print('Launching planner')
+                fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+                t = np.arange(0, 3, .01)
+                fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+                fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+                already_showed = True
 
                     # window.Refresh() if window else None        # yes, a 1-line if, so shoot me
 
         window.Close()
     except Exception as e:
         sg.popup_error_with_traceback(f'An error happened.  Here is the info:', e)
+    # if env_running:
+    #     p.kill()
+    #     os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+    kill_gazebo()
 
 if __name__ == '__main__':
     main()
